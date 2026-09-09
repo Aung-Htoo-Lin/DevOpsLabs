f0e5a4d805fe27c948fa9301c3058e49de35e40c
AungHtooLin <40841085@live.napier.ac.uk>
2026-09-09 19:31:56 +0630

    Added App.java
    Signed-off-by: aunghtoolin <aunghtoolin2007@gmail.com>

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -1,9 +1,7 @@
 package com.napier.devops;
 
-public class App
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) {
         System.out.println("Boo yah!");
     }
 }
